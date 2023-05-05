907153e10121c58bd9a686c6d1e2532a7cbab8b0
DaniJFerreira <daniel.jorge.ferreira1984@gmail.com>
2023-05-05 14:46:15 +0100

    create Destination and crew components as also the css style atribute for each element. implemented js code to render the elemwent on the usinge event listener and change the status of the section to replace each content information when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import Destination from './components/Destination';
+import Crew from './components/Crew';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Navbar/>
           <Routes basename ="space-tourism-project/">
            <Route path="space-tourism-project/" element={<Home />} />
+           <Route path="space-tourism-project/Destination" element={<Destination />} />
+           <Route path="space-tourism-project/Crew" element={<Crew />} />
           </Routes>
       </div>
     </Router>
